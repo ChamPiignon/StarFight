97226ac23a5a949dfbe42d77141e4ea99a51e9c0
flchaplais <florent.chaplais@etu.uca.fr>
2020-11-27 12:09:36 +0100

    + gamepad & stick
    Signed-off-by: flchaplais <florent.chaplais@etu.uca.fr>
    # traitement d'erreur pas fait
    # probleme avec la croix directionnelle

--- src/command/Command.java
+++ src/command/Command.java
@@ -1,5 +1,15 @@
 package command;
 
 
-public class Command {
+public abstract class Command {
+    public boolean up = false;
+    public boolean down = false;
+    public boolean left = false;
+    public boolean right = false;
+    public boolean btn_0 = false;
+    public boolean btn_1 = false;
+    public boolean btn_2 = false;
+    public boolean btn_3 = false;
+
+
 }
