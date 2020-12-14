6ef85d13f249f17d97b210691c83f42ab5e5c9af
flchaplais <florent.chaplais@etu.uca.fr>
2020-12-14 13:13:52 +0100

    fxml text -> bundle.getString()
    Signed-off-by: flchaplais <florent.chaplais@etu.uca.fr>

--- src/controllers/ChooseCharacter.java
+++ src/controllers/ChooseCharacter.java
@@ -2,23 +2,38 @@ package controllers;
 
 import character.Player;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Modality;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ResourceBundle;
 
-public class ChooseCharacter
-{
+public class ChooseCharacter {
     public ResourceBundle bundle = ResourceBundle.getBundle("domaine/properties/langue");
     boolean player1IsReady = false;
     boolean player2IsReady = false;
     private Player player1;
     private Player player2;
-    private Stage myStage;
+    private final Stage myStage;
+
+    @FXML
+    Text namePlayer1, namePlayer2;
+
+    @FXML
+    Button btn_left, btn_right;
+
+    @FXML
+    public void initialize() {
+        namePlayer1.setText(bundle.getString("NamePlayer1"));
+        namePlayer2.setText(bundle.getString("NamePlayer2"));
+        btn_left.setText(bundle.getString("Btn_ready"));
+        btn_right.setText(bundle.getString("Btn_ready"));
+    }
 
     public ChooseCharacter(Stage myStage) {
         this.myStage = myStage;
@@ -26,7 +41,7 @@ public class ChooseCharacter
 
     public void setReadyPlayer2(ActionEvent actionEvent) throws IOException {
         player2IsReady = true;
-        if(player1IsReady && player2IsReady)
+        if (player1IsReady && player2IsReady)
             launchGame();
     }
 
--- src/controllers/LaunchPage.java
+++ src/controllers/LaunchPage.java
@@ -1,9 +1,12 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -13,13 +16,29 @@ import java.util.ResourceBundle;
 public class LaunchPage {
     public ResourceBundle bundle = ResourceBundle.getBundle("domaine/properties/langue");
 
+    @FXML
+    TableColumn playerColumn, numberColumn, statColumn, bestColumn;
+
+    @FXML
+    Button btn_play, btn_exit;
+
+    @FXML
+    public void initialize() {
+        playerColumn.setText(bundle.getString("PlayerColumn"));
+        numberColumn.setText(bundle.getString("NumberColumn"));
+        statColumn.setText(bundle.getString("StatColumn"));
+        bestColumn.setText(bundle.getString("BestColumn"));
+        btn_play.setText(bundle.getString("Btn_play"));
+        btn_exit.setText(bundle.getString("Btn_exit"));
+    }
+
     public void onClickPlay(ActionEvent actionEvent) throws IOException {
         Stage primaryStage = new Stage();
         primaryStage.initModality(Modality.APPLICATION_MODAL);
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/view/chooseCharacter.fxml"));
         loader.setController(new ChooseCharacter(primaryStage));
         Parent root = loader.load();
-        Scene gameScene= new Scene(root);
+        Scene gameScene = new Scene(root);
         primaryStage.setTitle(bundle.getString("GameTitleGame"));
         primaryStage.setScene(gameScene);
         primaryStage.show();
