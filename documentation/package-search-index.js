d2aec3759fac91e0c123337c2868cc64c3e1add2
flchaplais <florent.chaplais@etu.uca.fr>
2021-01-19 14:43:57 +0100

    javadoc generated

---
+++ documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"character"},{"l":"com.company"},{"l":"command"},{"l":"controllers"},{"l":"domaine.properties"},{"l":"manager"},{"l":"persist"},{"l":"world"}]
